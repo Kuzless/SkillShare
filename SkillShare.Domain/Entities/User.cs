dab8c3053d11316ce133b7be369bf7258e899ca3
Kuzless <deadgoatbro@gmail.com>
2025-04-21 12:09:37 +0300

    Entities added

---
+++ SkillShare.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillShare.Domain.Entities
+{
+    public class User
+    {
+        Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+}
