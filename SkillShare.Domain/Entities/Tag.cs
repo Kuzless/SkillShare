467907b56326da20965c204e05476ba895ff10f4
Kuzless <deadgoatbro@gmail.com>
2025-04-21 12:19:20 +0300

    Added repository interfaces

--- SkillShare.Domain/Entities/Tag.cs
+++ SkillShare.Domain/Entities/Tag.cs
@@ -9,7 +9,7 @@ namespace SkillShare.Domain.Entities
     public class Tag
     {
         public int Id { get; set; }
+        public Guid UserId { get; set; }
         public string Name { get; set; }
-        public Guid UserId { get; set; } 
     }
 }
---
+++ SkillShare.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillShare.Domain.Interfaces
+{
+    public interface IGenericRepository
+    {
+        public Task<T> GetById<T>(ValueType id);
+        public Task<List<T>> GetAll<T>();
+        public Task Add<T>(T item);
+        public Task Update<T>(T item);
+        public Task Delete(ValueType id);
+    }
+}
