576c00125dc44bf4e7f6dcebac39f26af4218807
Kuzless <deadgoatbro@gmail.com>
2025-04-21 12:31:39 +0300

    Added context, registered it

--- SkillShare.API/Program.cs
+++ SkillShare.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SkillShare.Infrastructure;
+
 namespace SkillShare.API
 {
     public class Program
@@ -7,16 +10,17 @@ namespace SkillShare.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
---
+++ SkillShare.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillShare.Domain.Entities;
+
+namespace SkillShare.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users = null!;
+        public DbSet<Tag> Tags = null!;
+        public DatabaseContext() : base()
+        {
+            
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
